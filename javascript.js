3b1486ee9e710eee2d5653c3bce3432e08a35242
BStyvison <99987766+BStyvison@users.noreply.github.com>
2022-03-16 15:27:32 -0300

    atualizações

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+//Fullscreen Sticky Header
+
 window.addEventListener("scroll", function(){
     const header = this.document.querySelector('header')
     header.classList.toggle('sticky', window.scrollY > 0)
